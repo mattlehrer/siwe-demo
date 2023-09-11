7660dbe58c382ce8d6e7cb9511f17bb49a6b2ca5
Matt Lehrer <lehrerm@gmail.com>
2023-09-11 23:26:43 +0200

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,34 @@
 <script>
 	import Nav from '$lib/Nav.svelte';
 	import '../app.postcss';
+	import { signInWithCustomToken } from 'firebase/auth';
+	import { signerAddress } from '$lib/wagmi';
+	import { auth } from '$lib/firebase';
+	import { user } from '$lib/user';
+
+	$: if ($signerAddress) {
+		const token = fetch('/api/token', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ address: $signerAddress })
+		})
+			.then((res) => res.json())
+			.then((res) => res.customToken);
+		console.log({ token });
+
+		// signInWithCustomToken(auth, $signerAddress)
+		// 	.then((userCredential) => {
+		// 		// Signed in
+		// 		$user = userCredential.user;
+		// 		console.log('here', $user);
+		// 	})
+		// 	.catch((error) => {
+		// 		console.error(error);
+		// 		$user = null;
+		// 	});
+	}
 </script>
 
 <Nav />
