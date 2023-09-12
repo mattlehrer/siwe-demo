9b7386467b330c93b08624667b05a39dae8cf32b
Matt Lehrer <lehrerm@gmail.com>
2023-09-12 16:22:42 +0200

    connect button color

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -52,7 +52,7 @@
 								$loading = false;
 							}}
 							type="button"
-							class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							class="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
 							>{#if $loading}
 								Connecting...
 							{:else}
